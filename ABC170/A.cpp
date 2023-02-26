30bff5d0950b0ea319777f5b9f26c9e2b041e946
Takaaki Azakami <109128805+TakaakiAzakami@users.noreply.github.com>
2023-02-27 00:59:15 +0900

    Create A.cpp

---
+++ ABC170/A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep1(i,n) for(i=1;i<=n;i++)
+int main(){
+  int x[6];
+  int i;
+  
+  rep1(i,5){
+    cin >>x[i];
+    if(x[i]==0){
+      cout << i;
+    }
+  }
+  
+}
