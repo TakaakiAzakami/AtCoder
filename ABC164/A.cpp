0efe4cff2634dd4dc7647a7ebad069455ce02ddb
Takaaki Azakami <109128805+TakaakiAzakami@users.noreply.github.com>
2023-02-27 01:12:30 +0900

    Create A.cpp

---
+++ ABC164/A.cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+  int s;
+  int w;
+  
+  cin >> s >> w;
+  
+  if(s<=w)
+    cout << "unsafe" ;
+  else 
+    cout << "safe";
+    
+  
+}
