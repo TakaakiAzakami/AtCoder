ce9e142222bb4c4844fae5c68c83324f85c96fb1
Takaaki Azakami <109128805+TakaakiAzakami@users.noreply.github.com>
2023-02-27 01:07:51 +0900

    Create A.py

---
+++ ABC166/A.py
@@ -0,0 +1,17 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define rep0(i,n) for(i=0;i<n;i++)
+#define rep1(i,n) for(i=1;i<=n;i++)
+#define test(a) cout << "*" << a << endl
+#define ll long long
+int main(){
+  string s;
+  
+  cin >> s;
+  
+  if(s=="ABC")
+    cout << "ARC" << endl;
+  else if(s=="ARC")
+    cout << "ABC" << endl;
+  
+}
